068db4f36d84c11e06beb93de1ab8499b493c133
RITWIK MAL <ritwik.mal8@gmail.com>
2026-04-11 21:31:53 +0530

    connected live backend

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ function App() {
     setLoading(true);
 
     try {
-      const response = await fetch("http://localhost:5000/chat", {
+      const response = await fetch("https://medibot-backend-neuz.onrender.com/chat", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ messages: updatedMessages, patient }),
