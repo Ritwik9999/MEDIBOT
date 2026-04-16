c8b7ebd861edd63e0a0c33c35e1c649e96f1a721
RITWIK MAL <ritwik.mal8@gmail.com>
2026-04-17 01:26:55 +0530

    fix: remove temporary fix files

--- backend/fix5.js
+++
@@ -1,5 +0,0 @@
-const fs = require('fs');
-let content = fs.readFileSync('server.js', 'utf8');
-content = content.replace(/llama-3\.3-70b-versatile/g, 'deepseek-r1-distill-qwen-32b');
-fs.writeFileSync('server.js', content);
-console.log('Done!');
