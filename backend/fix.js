3a7c0711f61f3eeb14676a94602e9d2554f51fa1
RITWIK MAL <ritwik.mal8@gmail.com>
2026-04-17 00:52:13 +0530

    fix: remove temporary fix files

--- backend/fix.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-let content = fs.readFileSync('server.js', 'utf8');
-const oldLine = "const { buildRAGContext } = require('./rag');";
-const newLine = "const { buildRAGContext } = require('./rag');\nconst { updatePatientProfile, buildLongTermContext } = require('./longTermMemory');";
-content = content.replace(oldLine, newLine);
-fs.writeFileSync('server.js', content);
-console.log('Done!');
